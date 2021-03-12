80c22988fa5af8f573908baef65954087b514063
Merkury32 <pjozefiak32@gmail.com>
2021-03-12 15:56:01 +0100

    update project (add frameworks to components, add menu and layout components, service workers)

--- polyfills.js
+++ polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /work/road-fault-reporter/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /app/src/polyfills.ts */"hN/g");
 
 
 /***/ }),
